d67835db19652078daa1336d63cd171d48b2c110
munendralohia0071@gmail.com <munendralohia0071@gmail.com>
2026-01-05 17:02:07 +0530

    Your commit message

--- tests/e2e-suite.spec.ts
+++
@@ -1,7 +0,0 @@
-import '../tests/Home/Home.spec';
-import'../tests/auth/signup-login.spec';
-import '../tests/auth/login.spec';
-import '../tests/Product/product.spec';
-import '../tests/Cart/AddProducttoCart.spec';
-import '../tests/Cart/AddMultipleProducts.spec';
-import '../tests/E2EOrder/e2eorderplaces.spec';
